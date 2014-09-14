c80b11a47f53b55add99ba8ae26ecd89d9e71906
Jameson Lopp <jameson.lopp@gmail.com>
2014-09-14 14:22:57 -0400

    adding pid locking & disconnect call

--- node_connection_manager/node_rebalance.php
+++ node_connection_manager/node_rebalance.php
@@ -20,8 +20,26 @@ $config = array(
                     ),
                 );
 
+// don't allow multiple instances of this script to run simultaneously
+const PID_FILE = '/tmp/nodemanager.pid';
+
+if (file_exists(PID_FILE)) {
+	$pid = file_get_contents(PID_FILE);
+	if (file_exists("/proc/$pid")) {
+		error_log('An instance of the node manager script is already running.');
+		exit(1);
+	} else {
+		error_log('Previous process exited without cleaning pidfile.');
+		unlink(PID_FILE);
+	}
+}
+$h = fopen(PID_FILE, 'w');
+if ($h) {
+	fwrite($h, getmypid());
+}
+fclose($h);
+
 // build node IP whitelist from our pool of nodes
-// initialize connections to nodes
 $nodes = array();
 $whitelist = array();
 foreach ($config as $index => $node) {
@@ -30,11 +48,20 @@ foreach ($config as $index => $node) {
     $whitelist[$node['ip'] . ':8333'] = TRUE;
 }
 
+// query each node to check that it's online
+foreach ($nodes as $index => $node) {
+    if (FALSE === $node->getinfo()) {
+    	// node is offline; remove from list of nodes
+	    unset($nodes[$index]);
+    }
+}
+
 // query each node for all of its peers
 $connectedPeers = array();
 foreach ($nodes as $index => $node) {
     if (FALSE === $node->getpeerinfo()) {
-        echo 'ERROR getting peer info from ' . $node->getHost() . ':' . $node->getPort() . "\n";
+        echo "ERROR getting peer info from node $index\n";
+        echo "$node->error\n";
         continue;
     }
 
@@ -48,7 +75,15 @@ foreach ($nodes as $index => $node) {
         }
         
         if (array_key_exists($peer['addr'], $connectedPeers)) {
-            $node->addnode($peer['addr'], 'remove');
+            echo "Removing duplicate peer " . $peer['addr'] . "\n";
+            if (FALSE === $node->addnode($peer['addr'], 'disconnect')) {
+                echo "ERROR instructing node $index to disconnect peer " . $peer['addr'] . "\n";
+                echo "$node->error\n";
+            }
+            if (FALSE === $node->addnode($peer['addr'], 'remove')) {
+                //echo "ERROR instructing node $index to remove peer " . $peer['addr'] . "\n";
+                //echo "$node->error\n";
+            }
         } else {
             $connectedPeers[$peer['addr']] = TRUE;
         }
@@ -61,7 +96,21 @@ foreach ($nodes as $index => $node) {
             continue;
         }
 
-        $node->addnode($whitelist, 'onetry');
-        $node->addnode($whitelist, 'add');
+        // don't instruct the node to connect to a whitelisted peer if it's already connected
+        if (isset($whitelistPeers[$ip])) {
+            continue;
+        }
+
+        echo "Instructing node $index to connect to whitelist peer $ip\n";
+        if (FALSE === $node->addnode($ip, 'onetry')) {
+            echo "ERROR instructing node $index to connect to $ip\n";
+            echo "$node->error\n";
+        }
+        if (FALSE === $node->addnode($ip, 'add')) {
+            //echo "ERROR instructing node $index to add $ip\n";
+            //echo "$node->error\n";
+        }
     }
 }
+
+unlink(PID_FILE);
