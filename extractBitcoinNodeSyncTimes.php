3a1792bba9c9f17b7e2410439d37f779939bcd2a
Jameson Lopp <jameson@team.casa>
2021-10-27 09:11:14 -0400

    add sync log parsing script

---
+++ extractBitcoinNodeSyncTimes.php
@@ -0,0 +1,41 @@
+<?php
+
+// This script reads in a debug.log file produced by Bitcoin Core
+// and extracts block heights and how many minutes it took to sync to that point
+
+if (count($argv) < 2 || !$argv[1]) {
+    echo "ERROR: first argument must be path to debug log file\n";
+    exit;
+}
+
+$startTime;
+
+// output CSV file format: Minutes,Block Height
+echo "Minutes Syncing,Block Height\n";
+
+
+if (($handle = fopen($argv[1], "r")) !== FALSE) {
+    // we must find the first timestamp to determine the start time
+    while (($data = fgetcsv($handle, 1000, " ")) !== FALSE) {
+        if (isset($data[0]) && strtotime($data[0])) {
+            $startTime = strtotime($data[0]);
+            break;
+        }
+    }
+
+    while (($data = fgetcsv($handle, 1000, " ")) !== FALSE) {
+        if ($data[1] !== "UpdateTip:") {
+            continue;
+        }
+        $height = explode("=", $data[4])[1];
+        if ($height % 1000 != 0) {
+        	continue;
+        }
+
+        $syncTime = floor((strtotime($data[0]) - $startTime) / 60);
+        echo "$syncTime,$height\n";
+    }
+    fclose($handle);
+} else {
+	echo "ERROR: Failed to open debug.log file\n";
+}
