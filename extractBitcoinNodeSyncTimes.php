b3747aa9c5c6964b1e8d06f63201a9250b02206f
Jameson Lopp <jameson.lopp@gmail.com>
2022-02-25 13:59:13 -0500

    final bug fixes

--- extractBitcoinNodeSyncTimes.php
+++ extractBitcoinNodeSyncTimes.php
@@ -16,6 +16,7 @@ if (count($argv) < 3 || !is_numeric($argv[2]) || $argv[2] < 0) {
 $startTime;
 $version;
 $maxHeight = $argv[2];
+$heightField;
 
 // output CSV file format: Minutes,Block Height
 echo "Minutes Syncing,Block Height\n";
@@ -30,23 +31,27 @@ if (($handle = fopen($argv[1], "r")) !== FALSE) {
             // strip non-numeric chars to get the actual number
             $version = preg_replace("/[^0-9\.]/", "", $data[$position + 1]);
 
-            // if there is more than one decimal, truncate decimals so that we have a real number
-            preg_match_all('/\./', $version, $matches, PREG_OFFSET_CAPTURE);
-            if (array_key_exists(0, $matches) && array_key_exists(1, $matches[0]) && array_key_exists(1, $matches[0][1])) {
-                $version = substr($version, $matches[0][0][1] + 1, $matches[0][1][1] - $matches[0][0][1] - 1);
+            // if the version is the old style that's in the form of v0.something, truncate decimals so that we have a real number
+            if (intval($version) == 0) {
+                preg_match_all('/\./', $version, $matches, PREG_OFFSET_CAPTURE);
+
+                if (array_key_exists(0, $matches) && array_key_exists(1, $matches[0]) && array_key_exists(1, $matches[0][1])) {
+                    $version = substr($version, $matches[0][0][1] + 1, $matches[0][1][1] - $matches[0][0][1] - 1);
+                }                
+            } else {
+                $version = intval($version);
             }
             break;
         }
     }
 
-    // Bitcoin Core versions prior to v0.18 printed timestamp in YYY-MM-DD HH:MM:SS format
+    // Bitcoin Core versions prior to v0.17 printed timestamp in YYY-MM-DD HH:MM:SS format
     // while later versions used YYYY-MM-DDTHH:MM:SSZ format
     // This also shifts which CSV field various values end up in
-    $heightField = $version < 18 ? 6 : 4;
 
     // we must find the first timestamp to determine the start time
     while (($data = fgetcsv($handle, 1000, " ")) !== FALSE) {
-        if ($version < 18) {
+        if ($version < 17) {
             if (isset($data[0]) && strtotime($data[0] . " " . $data[1])) {
                 $startTime = strtotime($data[0] . " " . $data[1]);
                 break;
@@ -61,12 +66,20 @@ if (($handle = fopen($argv[1], "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, " ")) !== FALSE) {
         // Bitcoin Core versions prior to v0.9.0 printed block processed lines with "SetBestChain:"
         // while later versions use "UpdateTip:"
-        if ($version < 9 && $data[2] !== "SetBestChain:") {
+        if ($version < 9 && !in_array("SetBestChain:", $data)) {
             continue;
-        } else if ($version >= 9 && $version < 12 && $data[2] !== "UpdateTip:") {
+        } else if ($version >= 9 && !in_array("UpdateTip:", $data)) {
             continue;
-        } else if ($version >= 12 && $data[1] !== "UpdateTip:") {
-            continue;
+        }
+
+
+        // find the array value that includes the "height=" value; we only need to find and set this once since it's always the same
+        if (!isset($heightField)) {
+            foreach ($data as $index => $value) {
+                if (strstr($value, "height")) {
+                    $heightField = $index;
+                }
+            }
         }
 
         $height = explode("=", $data[$heightField])[1];
@@ -78,7 +91,7 @@ if (($handle = fopen($argv[1], "r")) !== FALSE) {
             break;
         }
 
-        if ($version < 12) {
+        if ($version < 17) {
             $syncTime = floor((strtotime($data[0] . " " . $data[1]) - $startTime) / 60);
         } else {
             $syncTime = floor((strtotime($data[0]) - $startTime) / 60);
