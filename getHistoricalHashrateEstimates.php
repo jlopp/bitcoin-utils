f4b3e7a8e3658f9c09d8f3735b7ea62565e20cf9
Jameson Lopp <jameson.lopp@gmail.com>
2023-04-12 10:48:55 -0400

    change CSV delimiter

--- getHistoricalHashrateEstimates.php
+++ getHistoricalHashrateEstimates.php
@@ -7,7 +7,7 @@ require 'vendor/autoload.php';
 
 use Denpa\Bitcoin\Client as BitcoinClient;
 
-$bitcoind = new BitcoinClient('http://qwerty:qwerty@localhost:8332/');
+$bitcoind = new BitcoinClient('http://username:password@localhost:8332/');
 $startHeight = $height = (int)$argv[1]; // start height
 $maxBlockHeight = (int)$argv[2];
 $heightRange = $maxBlockHeight - $height;
@@ -41,9 +41,11 @@ while ($height <= $maxBlockHeight) {
 // sort by height ascending
 ksort($estimates);
 
-echo "\nBlock Height,Trailing Blocks,Hashrate\n";
+echo "\nBlock Height|1|2|3|4|5|6|7|8|9|10|20|30|40|50|60|70|80|90|100|200|300|400|500|600|700|800|900|1000|2000|3000|4000|5000|6000|7000|8000|9000|10000\n";
 foreach ($estimates as $estimateHeight => $results) {
+	echo $estimateHeight;
 	foreach ($results as $target => $hashrate) {
-		echo "$estimateHeight,$target,$hashrate\n";
+		echo "|$hashrate";
 	}
+	echo "\n";
 }
