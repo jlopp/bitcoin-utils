a4d4a25e183598783025d84af8fab1d199ad5a41
Jameson Lopp <jameson.lopp@gmail.com>
2023-09-04 16:27:43 -0400

    extend trailing block parameter range

--- realtimeHashrate/outputEstimatedHashratePerBlock.php
+++ realtimeHashrate/outputEstimatedHashratePerBlock.php
@@ -6,12 +6,12 @@ use Denpa\Bitcoin\Client as BitcoinClient;
 // This script calls the Bitcoin Core "getnetworkhashps" RPC endpoint
 // for a given trailing number of blocks at each block height
 
-if (count($argv) < 2 || !$argv[1] || $argv[1] < 1 || $argv[1] > 1000) {
+if (count($argv) < 2 || !$argv[1] || $argv[1] < 1 || $argv[1] > 2000) {
     echo "ERROR: first argument must be an integer for trailing number of blocks used to estimate hashrate\n";
     exit;
 }
 
-$bitcoind = new BitcoinClient('http://qwerty:qwerty@localhost:8332/');
+$bitcoind = new BitcoinClient('http://user:pass@localhost:8332/');
 $startHeight = $height = 713762; // start height of realtime data
 $maxBlockHeight = $bitcoind->getBlockchaininfo()->get('blocks');
 $trailingBlocks = (int)$argv[1];
