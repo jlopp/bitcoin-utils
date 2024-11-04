018c7a466a31d53dbacb0900f30fa316ff24a330
Jameson Lopp <jameson.lopp@gmail.com>
2024-11-04 07:26:44 -0500

    improve script

--- getOFACBitcoinAddresses.php
+++ getOFACBitcoinAddresses.php
@@ -6,10 +6,16 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://www.treasury.gov/ofac/downloads/sdn.csv");
 curl_setopt($ch, CURLOPT_HEADER, 0);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // started redirecting to https://sanctionslistservice.ofac.treas.gov/api/publicationpreview/exports/sdn.csv
+curl_setopt($ch, CURLOPT_FAILONERROR, true); // Required for HTTP error codes to be reported via our call to curl_error($ch)
 
 $addresses = array();
 
 $result = curl_exec($ch);
+if (curl_errno($ch)) {
+    print_r(curl_error($ch));
+}
+curl_close($ch);
 
 // Look for instances of: XBT <address>;
 preg_match_all('/XBT \w+/', $result, $matches);
